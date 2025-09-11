1fe7847b14b00b0e51f66fe8a914a786a15cca11
naruapon <naruapon@webmail.npru.ac.th>
2025-09-11 22:59:34 +0700

    Remove Chat.md files and add new chat folder

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,15 @@
+// create page.tsx react
+'use client';
+
+import React from 'react';
+
+function ChatPage() {
+    return (
+        <div>
+        <h1>Chat Page</h1>
+        {/* Add your chat UI components here */}
+        </div>
+    );
+}
+
+export default ChatPage;
